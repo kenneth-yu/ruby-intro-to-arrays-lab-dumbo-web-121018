8c645bb03017dc52326d0d5c62f83328b954e90d
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-11-20 18:19:31 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -13,3 +13,19 @@ end
 def third_element(array)
   array[2]
 end
+
+def last_element(array)
+  array.pop
+end
+
+def first_element_with_array_methods(array)
+  array.shift
+end
+
+def last_element_with_array_methods
+  array.pop
+end
+
+def length_of_array(array)
+  array.length
+end
